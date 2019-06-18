98160ec53358c0e7ee15c66637b36a15a829eb99
shahram peyvandi <yasfuny@gmail.com>
2019-06-18 10:03:43 -0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,55 @@
+
+@extends('layout/master')
+@section('content')
+    
+    @include('partitions.preloader')
+    <!--Mainmenu-area-->
+    @include('partitions.menu')
+
+    <!--Mainmenu-area/-->
+    <!--Header-area-->
+    @include('partitions.header')
+
+    <!--Header-area/-->
+    <!--Feature-area-->
+    @include('partitions.Features')
+
+    <!--Feature-area/-->
+
+    @include('partitions.Servises')
+
+    @include('partitions.Special-Features')
+
+    @include('partitions.Prices')
+
+    @include('partitions.Team')
+
+
+    @include('partitions.Testimonial')
+
+
+    @include('partitions.faq')
+
+
+    @include('partitions.Clients')
+
+
+    @include('partitions.blogs')
+
+
+
+    @include('partitions.footer')
+
+
+
+
+
+
+
+
+
+    
+    @endsection
+
+
+
