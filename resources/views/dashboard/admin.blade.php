b05a406deb721347fdb4f8b9fb19cbbf8ab4c648
shahram peyvandi <yasfuny@gmail.com>
2019-06-19 11:17:22 -0700

    addmin panel

--- resources/views/dashboard/admin.blade.php
+++ resources/views/dashboard/admin.blade.php
@@ -1,23 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+<h2 class="lead text-center text-primary">نقشه سایت</h2>
+<hr>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
 
-                    this is admin
-                </div>
-            </div>
+
+<div class="card text-center my-4">
+    <div class="card-header text-white bg-secondary">
+      مدیریت منو
+    </div>
+   
+    <div class="card-footer text-left ">
+        <a href="#"><i class="fa fa-edit fa-2x mx-2" title="ویرایش"></i></a>
+        @if (true)
+        <a href=""><i class="fa fa-eye-slash fa-2x mx-1" title="عدم نمایش" aria-hidden="true"></i></a>
+            @else
+            <a href=""><i class="fa fa-eye fa-2x mx-1" title=" نمایش" aria-hidden="true"></i> </a>
+        @endif
+
+    </div>
+ </div>
+
+
+  <div class="card text-center">
+         <div class="card-header  text-white bg-secondary">
+                      مدیریت هدر
+        </div>
+            
+        <div class="card-footer text-left ">
+                <a href="#"><i class="fa fa-edit fa-2x mx-2" title="ویرایش"></i>
+                </a>
+                @if (true)
+                <a href=""><i class="fa fa-eye-slash fa-2x mx-1" title="عدم نمایش" aria-hidden="true"></i></a>
+            
+                @else
+                <a href=""><i class="fa fa-eye fa-2x mx-1" title=" نمایش" aria-hidden="true"></i>
+                </a>
+            
+            @endif
+    
         </div>
     </div>
-</div>
+
+    <div class="card text-center my-4">
+            <div class="card-header  text-white bg-secondary">
+                          مدیریت فوتر
+           </div>
+               
+           <div class="card-footer text-left ">
+                   <a href="#"><i class="fa fa-edit fa-2x mx-2" title="ویرایش"></i>
+                   </a>
+                   @if (true)
+                   <a href=""><i class="fa fa-eye-slash fa-2x mx-1" title="عدم نمایش" aria-hidden="true"></i></a>
+               
+                   @else
+                   <a href=""><i class="fa fa-eye fa-2x mx-1" title=" نمایش" aria-hidden="true"></i>
+                   </a>
+               
+               @endif
+       
+           </div>
+       </div>
+
+
+
+
+
+
 @endsection
